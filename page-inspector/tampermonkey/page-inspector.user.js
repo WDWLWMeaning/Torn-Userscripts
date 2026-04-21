0a1dc4a890dc03f6d56e0af73aa3c08cda1883a1
Kevin <kevin@er-ic.ca>
2026-04-21 06:13:21 +0000

    Bump userscript versions for favicon refresh

--- page-inspector/tampermonkey/page-inspector.user.js
+++ page-inspector/tampermonkey/page-inspector.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn Page Inspector
 // @namespace    torn-page-inspector
-// @version      1.1.0
+// @version      1.1.1
 // @description  Extract Torn page HTML, CSS, and computed styles with native Torn styling
 // @author       Kevin
 // @match        https://www.torn.com/*
